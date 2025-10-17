c32851c1a40db5c8385b7c47f7032bee69c4ae7a
Horisun-Site <horisunola@gmil.com>
2025-10-17 17:35:22 +0100

    more update for media view

--- src/Component/Navpc.jsx
+++ src/Component/Navpc.jsx
@@ -1,3 +1,144 @@
+// import React, { useState } from "react";
+// import logo from "../assets/logo.png";
+// import facebook from "../assets/facebook.png";
+// import twitter from "../assets/twitter.png";
+// import instagram from "../assets/instagram.png";
+// import social from "../assets/social.png";
+// import communication from "../assets/communication.png";
+// import well from "../assets/well.png";
+// import { Link } from "react-router-dom";
+// import { Menu, X } from "lucide-react";
+
+// const Navpc = () => {
+//   const [move, setMove] = useState(false);
+//   const [move1, setMove1] = useState(false);
+//   const [move2, setMove2] = useState(false);
+//   const [menuOpen, setMenuOpen] = useState(false);
+
+//   return (
+//     <nav className="fixed top-0 left-0 right-0 bg-[#18181a]/70 text-white z-50">
+//       <div className="flex items-center justify-between h-[110px] px-6 sm:px-8 lg:px-10 max-w-[1400px] mx-auto">
+              
+//         <div className="flex items-center justify-start h-[80%]">
+//           <img className="h-[60px] lg:h-[70px] w-auto" src={logo} alt="Logo" />
+//         </div>
+
+//         {/* Desktop Links - hidden below 1080px */}
+//         <ul className="hidden [@media(min-width:1081px)]:flex gap-6 items-center text-[15px]">
+//           <Link to="/">HOME</Link>
+//           <Link to="/about">ABOUT</Link>
+//           <Link to="/services">SERVICES</Link>
+//           <Link to="/shop">SHOP</Link>
+//           <Link to="/contact">CONTACT</Link>
+//         </ul>
+
+//         <div className="hidden [@media(min-width:1081px)]:flex items-center gap-4">
+//           {[
+//             {
+//               move,
+//               setMove,
+//               img: facebook,
+//               hoverImg: communication,
+//               label: "facebook",
+//               link: "https://web.facebook.com/scott.emmanuel.230253/followers",
+//             },
+//             {
+//               move: move1,
+//               setMove: setMove1,
+//               img: twitter,
+//               hoverImg: well,
+//               label: "twitter",
+//               link: "https://x.com/Horisun0",
+//             },
+//             {
+//               move: move2,
+//               setMove: setMove2,
+//               img: instagram,
+//               hoverImg: social,
+//               label: "instagram",
+//               link: "https://www.instagram.com/horisun4/",
+//             },
+//           ].map((icon, i) => (
+//             <div
+//               key={i}
+//               className="flex flex-col items-center justify-center w-[60px]"
+//               onMouseEnter={() => icon.setMove(true)}
+//               onMouseLeave={() => icon.setMove(false)}
+//             >
+//               {icon.move && (
+//                 <div className="h-[28px] w-[90px] flex rounded-[40px] items-center justify-center bg-[#4d61ff] mb-1">
+//                   <h1 className="text-[14px]">{icon.label}</h1>
+//                 </div>
+//               )}
+//               <div className="h-[45px] w-[45px] flex items-center justify-center rounded-full bg-[#222426]">
+//                 <a
+//                   href={icon.link}
+//                   rel="noopener noreferrer"
+//                   target="_blank"
+//                   className="flex items-center justify-center h-full w-full"
+//                 >
+//                   <img
+//                     className={`transition-all duration-300 ease-in-out ${
+//                       icon.move ? "h-[100%] w-[100%]" : "h-[45%] w-[45%]"
+//                     }`}
+//                     src={icon.move ? icon.hoverImg : icon.img}
+//                     alt={icon.label}
+//                   />
+//                 </a>
+//               </div>
+//             </div>
+//           ))}
+          
+//           <div className="rounded-[40px] bg-[#4d61ff] px-4 py-2 text-[15px] whitespace-nowrap">
+//             (+234) 916 529 7719
+//           </div>
+//         </div>
+        
+//         <div className="flex [@media(min-width:1081px)]:hidden items-center">
+//           {menuOpen ? (
+//             <X
+//               color="red"
+//               size={40}
+//               onClick={() => setMenuOpen(false)}
+//               className="cursor-pointer"
+//             />
+//           ) : (
+//             <Menu
+//               color="green"
+//               size={40}
+//               onClick={() => setMenuOpen(true)}
+//               className="cursor-pointer"
+//             />
+//           )}
+//         </div>
+//       </div>
+      
+//       {menuOpen && (
+//         <div
+//           className="flex flex-col items-center gap-6 text-[17px] py-10 bg-cover bg-center [@media(min-width:1081px)]:hidden"
+//           style={{
+//             backgroundImage:
+//               "url('https://xtratheme.com/elementor/business-2/wp-content/uploads/sites/18/2017/08/slider1-1-1.jpg')",
+//           }}
+//         >
+//           <Link to="/" onClick={() => setMenuOpen(false)}>HOME</Link>
+//           <Link to="/about" onClick={() => setMenuOpen(false)}>ABOUT</Link>
+//           <Link to="/services" onClick={() => setMenuOpen(false)}>SERVICES</Link>
+//           <Link to="/shop" onClick={() => setMenuOpen(false)}>SHOP</Link>
+//           <Link to="/contact" onClick={() => setMenuOpen(false)}>CONTACT</Link>
+//           <div className="rounded-[40px] bg-[#4d61ff] px-5 py-2 text-[15px]">
+//             (+234) 916 529 7719
+//           </div>
+//         </div>
+//       )}
+//     </nav>
+//   );
+// };
+
+// export default Navpc;
+
+
+
 import React, { useState } from "react";
 import logo from "../assets/logo.png";
 import facebook from "../assets/facebook.png";
@@ -15,15 +156,44 @@ const Navpc = () => {
   const [move2, setMove2] = useState(false);
   const [menuOpen, setMenuOpen] = useState(false);
 
+  // Define social icons in an array (to reuse for desktop + mobile)
+  const socialIcons = [
+    {
+      move,
+      setMove,
+      img: facebook,
+      hoverImg: communication,
+      label: "facebook",
+      link: "https://web.facebook.com/scott.emmanuel.230253/followers",
+    },
+    {
+      move: move1,
+      setMove: setMove1,
+      img: twitter,
+      hoverImg: well,
+      label: "twitter",
+      link: "https://x.com/Horisun0",
+    },
+    {
+      move: move2,
+      setMove: setMove2,
+      img: instagram,
+      hoverImg: social,
+      label: "instagram",
+      link: "https://www.instagram.com/horisun4/",
+    },
+  ];
+
   return (
     <nav className="fixed top-0 left-0 right-0 bg-[#18181a]/70 text-white z-50">
       <div className="flex items-center justify-between h-[110px] px-6 sm:px-8 lg:px-10 max-w-[1400px] mx-auto">
-              
+        
+        {/* --- LOGO --- */}
         <div className="flex items-center justify-start h-[80%]">
           <img className="h-[60px] lg:h-[70px] w-auto" src={logo} alt="Logo" />
         </div>
 
-        {/* Desktop Links - hidden below 1080px */}
+        {/* --- DESKTOP LINKS --- */}
         <ul className="hidden [@media(min-width:1081px)]:flex gap-6 items-center text-[15px]">
           <Link to="/">HOME</Link>
           <Link to="/about">ABOUT</Link>
@@ -32,33 +202,9 @@ const Navpc = () => {
           <Link to="/contact">CONTACT</Link>
         </ul>
 
+        {/* --- DESKTOP SOCIAL ICONS + PHONE --- */}
         <div className="hidden [@media(min-width:1081px)]:flex items-center gap-4">
-          {[
-            {
-              move,
-              setMove,
-              img: facebook,
-              hoverImg: communication,
-              label: "facebook",
-              link: "https://web.facebook.com/scott.emmanuel.230253/followers",
-            },
-            {
-              move: move1,
-              setMove: setMove1,
-              img: twitter,
-              hoverImg: well,
-              label: "twitter",
-              link: "https://x.com/Horisun0",
-            },
-            {
-              move: move2,
-              setMove: setMove2,
-              img: instagram,
-              hoverImg: social,
-              label: "instagram",
-              link: "https://www.instagram.com/horisun4/",
-            },
-          ].map((icon, i) => (
+          {socialIcons.map((icon, i) => (
             <div
               key={i}
               className="flex flex-col items-center justify-center w-[60px]"
@@ -66,8 +212,8 @@ const Navpc = () => {
               onMouseLeave={() => icon.setMove(false)}
             >
               {icon.move && (
-                <div className="h-[28px] w-[90px] flex rounded-[40px] items-center justify-center bg-[#4d61ff] mb-1">
-                  <h1 className="text-[14px]">{icon.label}</h1>
+                <div className="h-[28px] w-[90px] flex rounded-[40px] items-center justify-center bg-[#4d61ff] mb-1 transition-all duration-300">
+                  <h1 className="text-[14px] capitalize">{icon.label}</h1>
                 </div>
               )}
               <div className="h-[45px] w-[45px] flex items-center justify-center rounded-full bg-[#222426]">
@@ -88,12 +234,13 @@ const Navpc = () => {
               </div>
             </div>
           ))}
-          
+
           <div className="rounded-[40px] bg-[#4d61ff] px-4 py-2 text-[15px] whitespace-nowrap">
             (+234) 916 529 7719
           </div>
         </div>
-        
+
+        {/* --- MOBILE MENU ICON --- */}
         <div className="flex [@media(min-width:1081px)]:hidden items-center">
           {menuOpen ? (
             <X
@@ -112,7 +259,8 @@ const Navpc = () => {
           )}
         </div>
       </div>
-      
+
+      {/* --- MOBILE MENU CONTENT --- */}
       {menuOpen && (
         <div
           className="flex flex-col items-center gap-6 text-[17px] py-10 bg-cover bg-center [@media(min-width:1081px)]:hidden"
@@ -121,13 +269,42 @@ const Navpc = () => {
               "url('https://xtratheme.com/elementor/business-2/wp-content/uploads/sites/18/2017/08/slider1-1-1.jpg')",
           }}
         >
+          {/* Menu Links */}
           <Link to="/" onClick={() => setMenuOpen(false)}>HOME</Link>
           <Link to="/about" onClick={() => setMenuOpen(false)}>ABOUT</Link>
           <Link to="/services" onClick={() => setMenuOpen(false)}>SERVICES</Link>
           <Link to="/shop" onClick={() => setMenuOpen(false)}>SHOP</Link>
           <Link to="/contact" onClick={() => setMenuOpen(false)}>CONTACT</Link>
+
           <div className="rounded-[40px] bg-[#4d61ff] px-5 py-2 text-[15px]">
             (+234) 916 529 7719
+          </div>
+
+          {/* --- MOBILE SOCIAL ICONS --- */}
+          <div className="flex items-center justify-center gap-8 mt-6">
+            {socialIcons.map((icon, i) => (
+              <div
+                key={i}
+                className="h-[50px] w-[50px] flex items-center justify-center rounded-full bg-[#222426] hover:bg-[#4d61ff] transition-all duration-300"
+                onMouseEnter={() => icon.setMove(true)}
+                onMouseLeave={() => icon.setMove(false)}
+              >
+                <a
+                  href={icon.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="flex items-center justify-center h-full w-full"
+                >
+                  <img
+                    src={icon.move ? icon.hoverImg : icon.img}
+                    alt={icon.label}
+                    className={`object-contain transition-all duration-300 ${
+                      icon.move ? "h-[100%] w-[100%]" : "h-[50%] w-[50%]"
+                    }`}
+                  />
+                </a>
+              </div>
+            ))}
           </div>
         </div>
       )}
--- src/Component/Older.jsx
+++ src/Component/Older.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
+import like from "../assets/like.png";
 
 const Older = () => {
   return (
-    <div>Older</div>
-  )
-}
+    <div className="bg-[#15171a] min-h-screen flex flex-col items-center justify-center gap-[100px]">
+      <div className="flex flex-col md:flex-row items-center justify-center h-auto md:h-[40%] w-full px-4 md:px-0 text-center md:text-left">
+        
+        {/* Image block */}
+        <div className="h-[250px] sm:h-[300px] md:h-[90%] w-full md:w-[35%] mt-6 md:mt-0 flex items-center justify-center bg-[url('https://cdn.pixabay.com/photo/2017/09/04/22/53/red-2715835_960_720.jpg')] bg-no-repeat bg-cover bg-center rounded-[10px]">
+          <img
+            src="https://xtratheme.com/elementor/business-2/wp-content/uploads/sites/18/2017/11/tt1-1.jpg"
+            alt=""
+            className="w-[80%] md:w-[650px] rounded-[15px] mb-6 md:mb-[125px] md:ml-[100px] object-contain"
+          />
+        </div>
 
-export default Older
+        {/* Text Section */}
+        <div className="h-auto md:h-[90%] w-full md:w-[44%] md:ml-[50px] flex flex-col justify-center items-center md:items-start gap-6 mt-6 md:mt-0">
+          <div className="flex flex-col md:flex-row justify-center md:justify-start items-center gap-2">
+            <h2 className="text-[28px] sm:text-[32px] md:text-[40px] text-white font-bold">
+              Business
+            </h2>
+            <h2 className="text-[28px] sm:text-[32px] md:text-[40px] italic text-white">
+              Development Center
+            </h2>
+          </div>
+
+          <div className="px-2 sm:px-6 md:px-0">
+            <h3 className="text-[18px] sm:text-[20px] md:text-[24px] font-normal text-white leading-relaxed">
+              Xtra is redefining the marketplace for business purchases by <br className="hidden md:block" />
+              making it dramatically easier for buyers to discover, learn <br className="hidden md:block" />
+              more about, compare and buy the products and services they <br className="hidden md:block" />
+              need to run and grow their Businesses primarily through the <br className="hidden md:block" />
+              capacity building of intermediary service.
+            </h3>
+          </div>
+
+          <button className="h-[50px] w-[200px] rounded-[60px] bg-[#2294f2] hover:cursor-pointer text-white hover:text-[#2294f2] hover:bg-white font-bold text-[18px] flex items-center justify-center">
+            <div className="w-[25%] h-[100%] rounded-[100%] flex items-center justify-center">
+              <img className="h-[70%] w-[70%]" src={like} alt="" />
+            </div>
+            <div>GET STARTED</div>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Older;
